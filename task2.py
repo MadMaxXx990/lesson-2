dbe1b0c3893a01cc895a95571135756d255dd579
MadMaxXx990 <madmax098x@gmail.com>
2024-10-03 21:02:20 +0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,12 @@
+name = input("fruit")
+list = ["banana","apple","orange","mango"]
 
+if name == "banana" :
+    print("hello Banana")
+elif name == "apple" :
+    print ("hello apple")
+else :
+    print("no")
 
 
 
